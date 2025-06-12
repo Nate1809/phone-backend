b6489ced37ac07a83aa5687b1180dacbd991d2ee
Nathan Guzman <monone18@me.com>
2025-06-12 16:27:15 -0500

    3.22

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static('dist')) // this makes it so that we can access the files
 
 // Logging middleware using morgan
 morgan.token('body', (req) => {
-    return req.method === 'POST' ? JSON.stringify(req.body) : ''
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -22,126 +22,118 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 // GET all persons
 app.get('/api/persons', (request, response) => {
-    Person
-        .find({})
-        .then(persons => {
-            console.log("Got persons from database:", persons)
-            response.json(persons)
-        })
+  Person
+    .find({})
+    .then(persons => {
+      console.log('Got persons from database:', persons)
+      response.json(persons)
+    })
 })
 
 // GET a single person
-app.get('/api/persons/:id', (request, response) => {
-    Person
-        .findById(request.params.id)
-        .then(person => {
-            console.log("Got person from database:", person)
-            response.json(person)
-        })
-        .catch(error => next(error))
+app.get('/api/persons/:id', (request, response, next) => {
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      console.log('Got person from database:', person)
+      response.json(person)
+    })
+    .catch(error => next(error))
 })
 
 // GET info about the phonebook
 app.get('/info', (request, response) => {
-    Person.collection
-        .countDocuments()
-        .then(numEntries => {
-                console.log(`Phonebook has info for ${numEntries} people`)
-                response.send(
-                    `<p>Phonebook has info for ${numEntries} people</p>
+  Person.collection
+    .countDocuments()
+    .then(numEntries => {
+      console.log(`Phonebook has info for ${numEntries} people`)
+      response.send(
+        `<p>Phonebook has info for ${numEntries} people</p>
                         <p>${new Date()}</p>`
-                )
-            }
-        )
+      )
+    }
+    )
 })
 
 // DELETE a person
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then( () => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-const generateID = () => {
-    let newId
-    do {
-        newId = Math.floor(Math.random() * 1_000_000).toString()
-    } while (persons.find(person => person.id === newId))
-    return newId
-}
 
 //POST a new person
 app.post('/api/persons', (request, response, next) => {
-    console.log('POST request received')
-    const body = request.body
+  console.log('POST request received')
+  const body = request.body
 
-    // Check if the name and number are in the request body
-    // if (!body.name || !body.number) {
-    //     return response.status(400).json({
-    //         error: 'name and number are required'
-    //     })
-    // }
+  // Check if the name and number are in the request body
+  // if (!body.name || !body.number) {
+  //     return response.status(400).json({
+  //         error: 'name and number are required'
+  //     })
+  // }
 
-    // // Check if the name already exists
-    // if (persons.find(person => person.name === body.name)) {
-    //     return response.status(400).json({
-    //         error: 'name must be unique'
-    //     })
-    // }
+  // // Check if the name already exists
+  // if (persons.find(person => person.name === body.name)) {
+  //     return response.status(400).json({
+  //         error: 'name must be unique'
+  //     })
+  // }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then(savedPerson => {
+      console.log('Person saved to database')
+      response.json(savedPerson)
     })
-
-    person
-        .save()
-        .then(savedPerson => {
-            console.log('Person saved to database')
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 //Update single person
 app.put('/api/persons/:id', (request, response, next) => {
-    const {name, number} = request.body
+  const { name, number } = request.body
 
-    Person.findById(request.params.id)
-        .then(person => {
-            if(!person) {
-                return response.status(400).end()
-            }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(400).end()
+      }
 
-            person.name = name
-            person.number = number
-            return person.save().then(updatedPerson => {
-                response.json(updatedPerson)
-            })
-        })
-        .catch(error => next(error))
+      person.name = name
+      person.number = number
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 // Error handling middleware
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 // error handling middleware
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError'){
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 // this has to be the last loaded middleware, also all the routes should be registered before this!
@@ -150,5 +142,5 @@ app.use(errorHandler)
 // Finally, we need to start our server
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
